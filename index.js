5ff545636b3a9e892bfb82fa39e14201e82f5986
Akshita Bhardwaj <akshita.bhardwaj2006@gmail.com>
2026-04-01 23:00:10 +0530

    initial setup

---
+++ index.js
@@ -0,0 +1,23 @@
+const express=require("express");
+const app=express();
+const http=require("http");
+const {Server}=require("socket.io");
+const path=require("path");
+
+const server=http.createServer(app);  //we make server khud  se kyuki socket.io ko direct access chchaiye
+const io = new Server(server);     //also it attaches socket.io to the http server we made upar 
+const port=8080;       // io=socket.io server -> mangages all connected users(whole server)
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"/views",));
+app.use(express.static(path.join(__dirname,"/public",)));
+
+io.on('connection', (socket) => {                                           //listens for new user connection like app.use {on ->listns for event}
+    console.log("user connected");                                               //connection and disconnect is event
+    socket.on("disconnect",()=>{                                                //socket->specified user
+        console.log("user disconnected");
+    });
+});
+server.listen(port,()=>{
+    console.log("app is listening");
+});
